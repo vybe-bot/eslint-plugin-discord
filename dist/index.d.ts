be3e5d6cd7fea93fefe77eec4f2ffe2fc7b78fa9
Mostafa Essam <102423748+MrQuartz99@users.noreply.github.com>
2026-09-23 01:59:13 +0300

    remove dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { TSESLint } from '@typescript-eslint/utils';
-
-declare const plugin: TSESLint.FlatConfig.Plugin;
-declare const recommended: TSESLint.FlatConfig.ConfigArray;
-declare function createSharedProgramConfig(program: unknown, tsconfigRootDir?: string): TSESLint.FlatConfig.ConfigArray;
-
-export { createSharedProgramConfig, plugin as default, recommended };
